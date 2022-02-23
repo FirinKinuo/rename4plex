c8b6d2209646716ce1c833938cf8bb37afb51877
Firin Kinuo <deals@fkinuo.ru>
2022-02-23 23:58:55 +0300

    Add: Calling move functions from CLI

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,75 @@
 package main
 
 import (
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	log "github.com/sirupsen/logrus"
 	"go-plex-anime/internal/config"
+	"go-plex-anime/internal/filesystem/anime"
+	"go-plex-anime/internal/filesystem/movement"
+	"go-plex-anime/pkg/search"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
+
+type Opts struct {
+	Path         string `short:"p" long:"path" description:"Path to Anime folder or file" required:"true"`
+	SaveOriginal bool   `long:"save-original" description:"Keep the original file (Slower)" required:"false"`
+	SymLink      bool   `long:"try-symlink" description:"Use symlink instead of copy (Faster, but may not work on all OS)" required:"false"`
+}
+
+var opts Opts
+
+func replaceAnimeFile(animePath string, saveOriginal bool, useSymLink bool) (err error) {
+	if _, err := os.Stat(animePath); err == nil && search.IsExistString(filepath.Ext(animePath), &anime.VideoFormats) {
+		if animeFile, err := anime.InitFileAnime(opts.Path); err == nil {
+			if _, err := movement.MoveAnimeToPlex(animeFile, saveOriginal, useSymLink); err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+	return nil
+}
+
+func replaceAllAnimeInFolder(animeFolderPath string, saveOriginal bool, useSymLink bool) (err error) {
+	if videoFiles, err := ioutil.ReadDir(animeFolderPath); err == nil {
+		for _, videoFile := range videoFiles {
+			if animeFile, err := anime.InitFileAnime(filepath.FromSlash(fmt.Sprintf("%s/%s", animeFolderPath, videoFile.Name()))); err == nil {
+				if _, err := movement.MoveAnimeToPlex(animeFile, saveOriginal, useSymLink); err != nil {
+					return err
+				}
+			} else {
+				return err
+			}
+		}
+	} else {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
 	config.InitLogger()
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Debugf("Unable to determine arguments, reason: %s", err)
+		os.Exit(-1)
+	}
+	if animePath, err := os.Stat(opts.Path); err == nil {
+		if animePath.IsDir() {
+			if err := replaceAllAnimeInFolder(opts.Path, opts.SaveOriginal, opts.SymLink); err != nil {
+				log.Error(err)
+			}
+		} else {
+			if err := replaceAnimeFile(opts.Path, opts.SaveOriginal, opts.SymLink); err != nil {
+				log.Error(err)
+			}
+		}
+	} else {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/ilyakaznacheev/cleanenv v1.2.6
+	github.com/jessevdk/go-flags v1.5.0
 	github.com/oriser/regroup v0.0.0-20210730155327-fca8d7531263
 	github.com/sirupsen/logrus v1.8.1
 )
@@ -11,7 +12,7 @@ require (
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
